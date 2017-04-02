cd299592281ef6fa1ed080869834348cc0b72043
Onidoru <onidoru@yahoo.com>
2017-04-02 12:27:10 +0300

    Fix

--- main.rb
+++ main.rb
@@ -6,25 +6,31 @@ require 'sinatra/reloader'
 get('/styles.css') { scss :styles }
 
 get '/?' do
+  @title = 'Home'
   slim :home
 end
 
 get '/about/?' do
+  @title = 'About'
   slim :about
 end
 
 not_found do
+  @title = '4 Oh 4'
   slim :not_found
 end
 
 get '/lab1/?' do
+  @title = 'Lab 1'
   slim :lab1
 end
 
 get '/lab2/?' do
+  @title = 'Lab 2'
   slim :lab2
 end
 
 get '/lab3/?' do
+  @title = 'Lab 3'
   slim :lab3
 end
